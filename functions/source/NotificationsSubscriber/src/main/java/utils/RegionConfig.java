8b204778a8224023be8cf0ddb9be01e09cea1cf2
Federico Rodriguez <rodrifed@amazon.com>
2021-12-06 17:16:56 +0100

    Add NotificationsSubscriber Lambda function

---
+++ functions/source/NotificationsSubscriber/src/main/java/utils/RegionConfig.java
@@ -0,0 +1,12 @@
+package utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegionConfig {
+
+    public String awsRegion;
+    public String spApiEndpoint;
+}
